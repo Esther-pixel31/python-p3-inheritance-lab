776ef98eaa4fe858346a922854f585efb8080d86
EstherMutua <esthermutua16@gmail.com>
2025-05-13 16:20:48 +0300

    added functions

--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,17 @@ import random
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name, knowledge=None):
+        super().__init__(first_name, last_name)
+        if knowledge is None or not isinstance(knowledge, list) or len(knowledge) == 0:
+            self.knowledge = ["general pedagogy"]
+        else:
+            self.knowledge = knowledge
+
     def teach(self):
-        pass
+        """Returns a random piece of knowledge from the teacher's list."""
+        if not self.knowledge:
+             raise ValueError("Teacher has no knowledge to teach.")
+        return random.choice(self.knowledge)
+  
+        
